87723d8e9c266eb68eebcd2ca4c60c1a5a5269a4
LucasMorgani <morganis1411@gmail.com>
2025-05-13 11:31:29 -0300

    iniciando com o Ansible

--- main.tf
+++ main.tf
@@ -17,8 +17,14 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-0f9de6e2d2f067fca"
   instance_type = "t2.micro"
+ # user_data     = <<-EOF
+ #                    #!/bin/bash
+ #                    cd /home/ubuntu
+ #                    echo "<h1>Feito com Terraform</h1>" > index.html
+ #                    nohup busybox httpd -f -p 8080 &
+ #                     EOF
   key_name      = "KeyPair-Default"
   tags          = {
-    Name = "PrimeiraInstancia"
+    Name = "TesteAWS"
   }
 }
